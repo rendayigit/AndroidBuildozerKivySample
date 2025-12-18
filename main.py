e75236f7824aa58db97af1ad19ccfccbf9429ae2
rendayigit <renda.yigit@gmail.com>
2025-12-18 10:38:23 +0300

    separate kv file

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from kivy.app import App
 from kivy.utils import platform
 
 # ---------------------------------------------------------
-# SCENARIO 1: ON ANDROID (Standard Run, No Kaki)
+# SCENARIO 1: ON ANDROID (Standard Run)
 # ---------------------------------------------------------
 if platform == "android":
     import app_logic
+    from kivy.lang import Builder  # <--- ADD THIS
 
     class LiveApp(App):
         def build(self):
+            # <--- ADD THIS LINE
+            # Manually load the KV file since it doesn't match the App name
+            Builder.load_file("app_logic.kv")
+
             return app_logic.build_layout()
 
 
@@ -20,15 +25,14 @@ else:
     from kaki.app import App as KakiApp
 
     class LiveApp(KakiApp, App):
-        KV_FILES = []
+        # Kaki loads this automatically on PC
+        KV_FILES = ["app_logic.kv"]
         CLASSES = {"LiveLayout": "app_logic"}
         AUTORELOADER_PATHS = [(os.getcwd(), {"recursive": True})]
 
         def build_app(self, first=False):
             try:
                 import app_logic
-
-                # Force reload the module so changes appear
                 import importlib
 
                 importlib.reload(app_logic)
